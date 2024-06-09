1072d321ff5b07d378f2f6141e4b09fe90c4457b
kukestan <kukestan@163.com>
2024-06-09 20:16:27 +0800

    update

---
+++ book1/Chapter2/lesson3/2-3-1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	char c;
+	cin >> c;
+	c = c + 32;
+	cout << c << endl;
+
+	return 0;
+}
---
+++ book1/Chapter2/lesson3/2-3-XT-1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	char c;
+	cin >> c;
+	cout << c - 'a' + 1<< endl;
+	
+	return 0;
+}
